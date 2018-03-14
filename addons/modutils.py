ebc17696cd080a8ec9a0e6fc0911c2ac96e118dd
dj505 <dj505@users.noreply.github.com>
2018-03-14 11:27:40 -0600

    Fixed path to bean.py

--- addons/modutils.py
+++ addons/modutils.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from configparser import SafeConfigParser
 import datetime
 import asyncio
-from bean.py import parser
+from addons//bean.py import parser
 
 class ModUtils:
     '''
